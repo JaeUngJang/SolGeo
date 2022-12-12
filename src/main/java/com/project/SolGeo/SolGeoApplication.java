a0c6aaed03c1049c7c5021e6bac1484558b6c542
Jae-Ung-Code <jjw3435@naver.com>
2022-12-12 19:47:07 +0900

    init

---
+++ src/main/java/com/project/SolGeo/SolGeoApplication.java
@@ -0,0 +1,13 @@
+package com.project.SolGeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolGeoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolGeoApplication.class, args);
+	}
+
+}
