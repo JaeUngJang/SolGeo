2d594d90c8c159b5641ba5dd8b643b42962f534e
Jae-Ung-Code <jjw3435@naver.com>
2022-12-21 22:27:12 +0900

    test: admin crud

--- src/test/java/project/solgeo/repository/AdminMapperTest.java
+++ src/test/java/project/solgeo/repository/AdminMapperTest.java
@@ -1,5 +1,6 @@
 package project.solgeo.repository;
 
+import org.apache.ibatis.annotations.Param;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,10 +8,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import project.solgeo.domain.Admin;
+import project.solgeo.repository.dto.AdminDto;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 
 @SpringBootTest
@@ -25,21 +29,55 @@ public class AdminMapperTest {
     @DisplayName("Save Admin")
     void save() {
         // given
-        Admin admin = Admin.builder()
-                .adminName("admin")
-                .adminId("adminId")
-                .adminPassword("adminPassword")
-                .adminPhoneNumber("adminPhoneNumber")
-                .adminSex(true)  //true-Male, false-female
-                .build();
+        String adminName = "admin";
+        String adminId = "adminId";
+        String adminPassword = "adminPassword";
+        String adminPhoneNumber = "adminPhoneNumber";
+        Boolean adminSex = true;  //true-Male, false-female
 
-        // when
-        adminMapper.save(admin);
+        adminSave(adminName, adminId, adminPassword, adminPhoneNumber, adminSex);
     }
 
     @Test
-    @DisplayName("Select Admin")
-    void select() {
+    @DisplayName("Find All Admin")
+    void findAll() {
+        // given
+        String adminName = "admin";
+        String adminId = "adminId";
+        String adminPassword = "adminPassword";
+        String adminPhoneNumber = "adminPhoneNumber";
+        Boolean adminSex = true;  //true-Male, false-female
+        Admin admin = adminSave(adminName, adminId, adminPassword, adminPhoneNumber, adminSex);
+
+        // when
+        // 모든 정보 입력 검증
+        test(null, adminName, adminId, adminPhoneNumber, admin);
+        // adminName 입력
+        test(null, "dm", null, null, admin);
+    }
+
+    @Test
+    @DisplayName("Find by Admin Id")
+    void findByAdminId() {
+        // given
+        String adminName = "admin";
+        String adminId = "adminId";
+        String adminPassword = "adminPassword";
+        String adminPhoneNumber = "adminPhoneNumber";
+        Boolean adminSex = true;  //true-Male, false-female
+        Admin admin = adminSave(adminName, adminId, adminPassword, adminPhoneNumber, adminSex);
+
+        // when
+        Optional<Admin> selectedAdmin = adminMapper.findByAdminId(admin.getAdminId());
+
+        // then
+        assertThat(admin.getId()).isEqualTo(selectedAdmin.get().getId());
+    }
+
+
+    @Test
+    @DisplayName("Update Admin")
+    void update() {
         // given
         String adminName = "admin";
         String adminId = "adminId";
@@ -50,12 +88,52 @@ public class AdminMapperTest {
         Admin admin = adminSave(adminName, adminId, adminPassword, adminPhoneNumber, adminSex);
 
         // when
-        // 모든 정보 입력 검증
-        test(null, adminName, adminId, adminPhoneNumber, admin);
-        // adminName 입력
-        test(null, "dm", null, null, admin);
+        String updatedAdminName = "updatedAdmin";
+        String updatedAdminId = "updatedAdminId";
+        String updatedAdminPassword = "updatedAdminPassword";
+        String updatedAdminPhoneNumber = "updatedAdminPN";
+        Boolean updatedAdminSex = false;  //true-Male, false-female
 
+        AdminDto adminDto = new AdminDto(updatedAdminName, updatedAdminId, updatedAdminPassword, updatedAdminPhoneNumber, updatedAdminSex);
+        adminMapper.update(admin.getId(), adminDto);
+
+
+        // then
+        Optional<Admin> updatedAdmin = adminMapper.findByAdminId(updatedAdminId);
+        Long id = updatedAdmin.get().getId();
+        Long id1 = admin.getId();
+        assertThat(updatedAdmin.get().getId()).isEqualTo(admin.getId());
     }
+
+    @Test
+    @DisplayName("Delete Admin")
+    void delete() {
+        // given
+        String adminName = "admin";
+        String adminId = "adminId";
+        String adminPassword = "adminPassword";
+        String adminPhoneNumber = "adminPhoneNumber";
+        Boolean adminSex = true;  //true-Male, false-female
+
+        Admin admin = adminSave(adminName, adminId, adminPassword, adminPhoneNumber, adminSex);
+
+        // when
+        adminMapper.delete(admin.getId());
+
+        // then
+        Admin selectedAdmin = Admin.builder()
+                .adminId("adminId")
+                .build();
+
+        List<Admin> allAdmin = adminMapper.findAll(selectedAdmin);
+
+        assertThatThrownBy(() -> allAdmin.get(0)).isInstanceOf(IndexOutOfBoundsException.class);
+    }
+
+
+
+
+
 
     Admin adminSave(String adminName, String adminId, String adminPassword, String adminPhoneNumber, Boolean adminSex) {
         Admin admin = Admin.builder()
@@ -69,6 +147,14 @@ public class AdminMapperTest {
         return admin;
     }
 
+    /**
+     * id, adminName, adminId, adminPhoneNumber 으로 Admin select
+     * @param id
+     * @param adminName
+     * @param adminId
+     * @param adminPhoneNumber
+     * @param admins
+     */
     void test(Long id, String adminName, String adminId, String adminPhoneNumber, Admin... admins) {
         Admin selectAdmin = Admin.builder()
                 .id(id)
