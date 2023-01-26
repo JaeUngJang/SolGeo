478acf3f4cc5727b7c9f05ea95e88df7e182307f
Jae-Ung-Code <jjw3435@naver.com>
2023-01-26 17:32:01 +0900

    refactor: change floor DB

--- src/main/java/project/solgeo/domain/Floor.java
+++ src/main/java/project/solgeo/domain/Floor.java
@@ -1,2 +1,23 @@
-package project.solgeo.domain;public class Floor {
+package project.solgeo.domain;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+public class Floor {
+
+    // PK
+    private Long id;
+    private Integer number;
+
+    //FK
+    private Long buildingId;
+
+    @Builder
+    public Floor(Long id, Integer number, Long buildingId) {
+        this.id = id;
+        this.number = number;
+        this.buildingId = buildingId;
+    }
+
 }
