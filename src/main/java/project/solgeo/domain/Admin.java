c346e674701880edc5db0a90011a9c07db71a41e
Jae-Ung-Code <jjw3435@naver.com>
2023-01-26 16:26:39 +0900

    refactor: chagne admin DB

--- src/main/java/project/solgeo/domain/Admin.java
+++ src/main/java/project/solgeo/domain/Admin.java
@@ -4,33 +4,41 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @NoArgsConstructor
 public class Admin {
 
     // PK
     private Long id;
-    private String adminName;
+    private LocalDateTime createdDate;
+    private LocalDateTime lastModifiedDate;
+    private String name;
     private String adminId;
-    private String adminPassword;
-    private String adminPhoneNumber;
-    private Boolean adminSex;
+    private String password;
+    private String phone;
+    private Boolean sex;
 
     @Builder
-    public Admin(Long id, String adminName, String adminId, String adminPassword, String adminPhoneNumber, Boolean adminSex) {
+    public Admin(Long id, LocalDateTime createdDate, LocalDateTime lastModifiedDate, String name, String adminId, String password, String phone, Boolean sex) {
         this.id = id;
-        this.adminName = adminName;
+        this.createdDate = createdDate;
+        this.lastModifiedDate = lastModifiedDate;
+        this.name = name;
         this.adminId = adminId;
-        this.adminPassword = adminPassword;
-        this.adminPhoneNumber = adminPhoneNumber;
-        this.adminSex = adminSex;
+        this.password = password;
+        this.phone = phone;
+        this.sex = sex;
     }
 
-    public Admin(Long id, String adminName, String adminId, String adminPhoneNumber, Boolean adminSex) {
+    public Admin(Long id, LocalDateTime createdDate, LocalDateTime lastModifiedDate, String name, String adminId, String phone, Boolean sex) {
         this.id = id;
-        this.adminName = adminName;
+        this.createdDate = createdDate;
+        this.lastModifiedDate = lastModifiedDate;
+        this.name = name;
         this.adminId = adminId;
-        this.adminPhoneNumber = adminPhoneNumber;
-        this.adminSex = adminSex;
+        this.phone = phone;
+        this.sex = sex;
     }
 }
