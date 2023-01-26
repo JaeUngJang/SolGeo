51a13f86b6dcf848848789cbf70264e5802f37dc
Jae-Ung-Code <jjw3435@naver.com>
2023-01-26 17:32:33 +0900

    refactor: change kitchenReservation DB

--- src/main/java/project/solgeo/domain/KitchenReservation.java
+++ src/main/java/project/solgeo/domain/KitchenReservation.java
@@ -12,14 +12,20 @@ public class KitchenReservation {
     // PK
     private Long id;
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdDate;
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime lastModifiedDate;
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime date;
     // FK
     private Long residentId;
 
 
     @Builder
-    public KitchenReservation(Long id, LocalDateTime date, Long residentId) {
+    public KitchenReservation(Long id, LocalDateTime createdDate, LocalDateTime lastModifiedDate, LocalDateTime date, Long residentId) {
         this.id = id;
+        this.createdDate = createdDate;
+        this.lastModifiedDate = lastModifiedDate;
         this.date = date;
         this.residentId = residentId;
     }
