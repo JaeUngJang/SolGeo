8886e8e17481bbaf20cc64a244063dcf2fc8a035
Jae-Ung-Code <jjw3435@naver.com>
2023-01-26 17:33:44 +0900

    refactor: change room DB

--- src/main/java/project/solgeo/domain/Room.java
+++ src/main/java/project/solgeo/domain/Room.java
@@ -3,6 +3,9 @@ package project.solgeo.domain;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
@@ -10,17 +13,25 @@ public class Room {
 
     //PK
     private Long id;
-    private Integer roomFloor;
-    private Integer roomNo;
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdDate;
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime lastModifiedDate;
+    private Integer number;
+    private Integer peopleNum;
     //FK
+    private Long floorId;
     private Long buildingId;
 
 
     @Builder
-    public Room(Long id, Integer roomFloor, Integer roomNo, Long buildingId) {
+    public Room(Long id, LocalDateTime createdDate, LocalDateTime lastModifiedDate, Integer number, Integer peopleNum, Long floorId, Long buildingId) {
         this.id = id;
-        this.roomFloor = roomFloor;
-        this.roomNo = roomNo;
+        this.createdDate = createdDate;
+        this.lastModifiedDate = lastModifiedDate;
+        this.number = number;
+        this.peopleNum = peopleNum;
+        this.floorId = floorId;
         this.buildingId = buildingId;
     }
 }
